fdaa527a1e014905037931fbcb91026bb4b09d61
chisty sohel <chistysohail@gmail.com>
2022-11-10 09:48:04 -0500

    initial

---
+++ Chisty.Models.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Chisty.Models.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Chisty.Services.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Chisty.Services.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
